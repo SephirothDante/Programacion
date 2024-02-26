cb12b925532adf3b5da46db17946888697aa431b
SephirothDante <fforetro@gmail.com>
2024-02-25 22:58:48 -0600

    Creating java class

---
+++ src/main/java/com/example/demo/prueba1/StudentsController.java
@@ -0,0 +1,20 @@
+package com.example.demo.prueba1;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(path = "/api/v1/student")
+public class StudentsController {
+    @GetMapping
+    public List<Student> getStudent() {
+        return List.of(
+            new Student(1L, "Fespi", "fforetro@gmai.com", 35, LocalDate.of(1987, Month.APRIL, 16))
+        );
+    }
+}
