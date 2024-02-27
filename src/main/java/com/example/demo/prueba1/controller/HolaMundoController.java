58e2e39f4d6187bb9e734ae4e1b804bf52d59f84
Sephiroth Dante <fforetro@gmail.com>
2024-02-27 12:46:55 -0600

    Cambios menores

--- src/main/java/com/example/demo/prueba1/controller/HolaMundoController.java
+++ src/main/java/com/example/demo/prueba1/controller/HolaMundoController.java
@@ -3,7 +3,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/say")
 public class HolaMundoController {
