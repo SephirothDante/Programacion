452256e05dd0b41c1b3bd04fdfcc9002acbe4827
Sephiroth Dante <fforetro@gmail.com>
2024-03-12 13:12:24 -0600

    creando base personajes

---
+++ src/main/java/com/example/demo/Personajes/personajeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.Personajes;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/personajes")
+public class personajeController {
+@GetMapping
+public List<personaje> getPersonaje(){
+    return List.of(
+        new personaje(1l, "Cloud", 21, "Midgard")
+    );
+}
+}
